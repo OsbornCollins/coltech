90b471cdfd0d925d0dd3565a34a902680c303287
OsbornCollins <2012111357@ub.edu.bz>
2022-12-01 21:37:25 -0600

    Added CORS functionality

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -40,6 +41,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 // Dependency Injection
@@ -71,6 +75,12 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "30ebafc237533c", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "ce73e749caad00", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Coltech <no-reply@coltech.osborncollins.net>", "SMTP sender")
+	// Use the flag.Func() function to parse our trusted origins flag from
+	// a string to a slice of string
+	flag.Func("cors-trusted-origin", "Trusted CORS origin (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
